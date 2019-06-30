b8ce2922695448adc1c856b8ec1c860f08b42c5c
Michael Mansy <michael.mansy@yahoo.ca>
2019-06-30 11:43:14 -0400

    create and update profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const auth = require('../../middleware/auth');
 
+const { check, validationResult } = require('express-validator/check');
+
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -23,5 +25,56 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+
+//@route     POST api/profile
+//@desc      create and update user profile
+//@access    Private
+router.post('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skills => skills.trim());
+    }
+
+    console.log(profileFields.skills);
+
+    res.send('Hello');
+});
+
+
+
+
+
 
 module.exports = router;
