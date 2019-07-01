b4af4ce6e1cb1ac058dc56616cb7777b4883b27a
Michael Mansy <michael.mansy@yahoo.ca>
2019-07-01 10:08:20 -0400

    get all profiles & profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -67,9 +67,72 @@ router.post('/', [auth, [
         profileFields.skills = skills.split(',').map(skills => skills.trim());
     }
 
-    console.log(profileFields.skills);
+    //build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
 
-    res.send('Hello');
+    try {
+        let profile = await Profile.findOne({ user: req.user.id });
+
+        if (profile) {
+            //update
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+            );
+
+            return res.json(profile);
+        }
+
+        //if profile does not already exist; create a new one
+        profile = new Profile(profileFields);
+
+        await profile.save();
+        res.json(profile);
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+//@route     GET api/profile
+//@desc      Get all profiles
+//@access    Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+//@route     GET api/profile/user/:user_id
+//@desc      Get profile by user ID
+//@access    Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectID') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
 });
 
 
